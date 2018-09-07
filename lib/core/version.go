6303cd72ee6d8489579bf1f966bdbc60369e3169
enr <enrico.comiti@gmail.com>
2018-09-07 15:17:43 +0200

    introduce deps

---
+++ lib/core/version.go
@@ -0,0 +1,10 @@
+package core
+
+// Version of the running app.
+var Version = ""
+
+// BuildTime of the running app.
+var BuildTime = ""
+
+// GitCommit is the Git hash and a "+CHANGES" suffix if there are uncommitted changes.
+var GitCommit = ""
