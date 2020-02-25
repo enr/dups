683aa290ace7f7cf7c117f51813da39b3746f4b5
enr <enrico.comiti@gmail.com>
2020-02-25 22:48:05 +0100

    fix tests

--- cmd/dups/dups_e2e_test.go
+++ cmd/dups/dups_e2e_test.go
@@ -2,38 +2,7 @@ package main
 
 import (
 	"testing"
-
-	"github.com/enr/runcmd"
 )
-
-var executions = []commandExecution{
-	{
-		Command: &runcmd.Command{
-			Exe:  "../../bin/dups",
-			Args: []string{"../../testdata/01"},
-		},
-		Success:  false,
-		ExitCode: 2,
-		Stdout:   "f1d2d2f924e986ac86fdf7b36c94bcdf32beec15 sub/010.txt",
-	},
-	{
-		Command: &runcmd.Command{
-			Exe:  "../../bin/dups",
-			Args: []string{"--names-only", "../../testdata/01"},
-		},
-		Success:  false,
-		ExitCode: 2,
-		Stdout:   "sub/010.txt",
-	},
-	{
-		Command: &runcmd.Command{
-			Exe:  "../../bin/dups",
-			Args: []string{},
-		},
-		Success:  false,
-		ExitCode: 1,
-	},
-}
 
 func TestCommandExecution(t *testing.T) {
 	verifyExecutions(t, executions)
